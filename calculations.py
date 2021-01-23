3fc8d9453c0a1e7a99388e1debf806874db06954
Lakee Sivaraya <lakeesivaraya@gmail.com>
2021-01-23 15:53:14 +0000

    calculation description

---
+++ calculations.py
@@ -0,0 +1,21 @@
+"""
+we want a function that takes lists of
+alcoholic drink,
+time,
+volume,
+
+example list
+
+ [[vodka, 11:00, 10], [beer, 12:00, 100]]
+
+then it returns a list of
+time
+Blood alcohol level over time
+
+example
+
+t= [11:00,11:30,12:00]
+BAC = [0.01,0.005,0.2]
+
+
+"""
